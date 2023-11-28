37ac577d1839cdd25790c783beb5659ddc41b7eb
Dingpals <150411456+Dingpals@users.noreply.github.com>
2023-11-28 15:11:25 +0800

    Add files via upload

---
+++ RegistrationForm/src/main/java/Username.java
@@ -0,0 +1,23 @@
+
+import java.awt.Color;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author WS
+ */
+class Username {
+
+    static String getText() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    static void setBackground(Color RED) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
